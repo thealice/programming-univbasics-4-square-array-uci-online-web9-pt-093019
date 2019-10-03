fca59f84441a347bd720ff312aaf651cef0e8ebe
Alice Freda <alicefreda@gmail.com>
2019-10-03 19:43:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0
  
   while counter < numbers.length do
-    new_numbers = numbers[counter]** numbers[counter]
+    new_numbers = [numbers(counter) ** numbers(counter)]
     puts new_numbers[counter]
     counter += 1
   end
