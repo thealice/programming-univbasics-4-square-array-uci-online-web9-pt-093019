eeb6802e8c52d88c06ef5f05b47fa117a67a7bb1
Alice Freda <alicefreda@gmail.com>
2019-10-03 19:56:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
-def square_array(num1, num2, num3)
+def square_array(numbers)
   counter = 0
-  numbers = [num1, num2, num3]
+  numbers = ["#{numbers}"]
   while counter < numbers.length do
     new_numbers = [numbers(counter) ** numbers(counter)]
     puts new_numbers
